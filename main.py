5145e4460ea1f0478192ca96f42eafc1dffccbc2
davidvujic <david@vujic.se>
2021-10-03 19:52:16 +0200

    wrap execution in functions, one with pipes and the other one with functions executed one by one

--- main.py
+++ main.py
@@ -22,10 +22,27 @@ def fn4(args) -> str:
     return f"{args} Hello World"
 
 
-res = funcs.pipe(fn1, fn2, fn3, fn4)
+def print_result(res):
+    if result.has_failed(res):
+        print(f"Failed at: {res.fn.__name__}. Exception? {res.exception}")
+    else:
+        print(f"The result is: {res} and the type is {type(res)}")
 
 
-if result.has_failed(res):
-    print(f"Failed at: {res.fn.__name__}. Exception? {res.exception}")
-else:
-    print(f"The result is: {res} and the type is {type(res)}")
+def with_piped_functions():
+    res = funcs.pipe(fn1, fn2, fn3, fn4)
+
+    print_result(res)
+
+
+def with_functions_one_by_one():
+    a = fn1(None)
+    b = fn2(a)
+    c = fn3(b)
+    d = fn4(c)
+
+    print_result(d)
+
+
+# with_piped_functions()
+# with_functions_one_by_one()
