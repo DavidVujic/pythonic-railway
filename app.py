599d9ce277f3088dde7a3909101c0b7ca43badda
davidvujic <david@vujic.se>
2021-10-04 14:10:17 +0200

    refactor: move functions from move.py to app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from rails import railway, funcs
+import csv
+
+
+@railway.tracks
+def _parse(path_to_csv_file):
+    with open(path_to_csv_file, mode="r") as f:
+        return list(csv.DictReader(f))
+
+
+@railway.tracks
+def _get_headers(data):
+    return data[0].keys()
+
+
+@railway.true_false_tracks
+def _has_valid_headers(headers):
+    true_or_false = map(lambda header: True if header else False, headers)
+
+    return False not in set(true_or_false)
+
+
+def run(path):
+    return funcs.pipe(path, _parse, _get_headers, _has_valid_headers)
+
+
+def run_each_function_one_by_one(path):
+    data = _parse(path)
+    headers = _get_headers(data)
+    is_valid = _has_valid_headers(headers)
+
+    return is_valid
--- main.py
+++ main.py
@@ -1,47 +1,23 @@
-from rails import railway, result, funcs
-import csv
+import sys
+import app
+from rails import result
 
-CSV_PATH = "./data/example.csv"
-
-
-@railway.tracks
-def parse(path_to_csv_file):
-    with open(path_to_csv_file, mode="r") as f:
-        return list(csv.DictReader(f))
+default_path = "./data/example.csv"
 
 
-@railway.tracks
-def get_headers(data):
-    return data[0].keys()
+def main(path):
+    res = app.run(path)
 
-
-@railway.true_false_tracks
-def has_valid_headers(headers):
-    true_or_false = map(lambda header: True if header else False, headers)
-
-    return False not in set(true_or_false)
-
-
-def print_result(res):
     if result.has_failed(res):
-        print(f"Failed at: {res.fn.__name__}. Exception? {res.exception}")
+        return (
+            f"Failed at: {res.fn.__name__}. "
+            f"{repr(res.exception) if res.exception else ''}"
+        )
     else:
-        print(f"The result is: {res} and the type is {type(res)}")
+        return f"The result is: {res} and the type is {type(res)}"
 
 
-def with_piped_functions():
-    res = funcs.pipe(CSV_PATH, parse, get_headers, has_valid_headers)
-
-    print_result(res)
-
-
-def with_functions_one_by_one():
-    data = parse(CSV_PATH)
-    headers = get_headers(data)
-    is_valid = has_valid_headers(headers)
-
-    print_result(is_valid)
-
-
-with_piped_functions()
-with_functions_one_by_one()
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) >= 2 else default_path
+    res = main(path)
+    print(res)
